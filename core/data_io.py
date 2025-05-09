bd347f78fe9a1556f3d1a03ac1c1b09cd453bc2c
MLAcikgoz <acikgozmehmetlutfu34@gmail.com>
2025-05-09 02:31:40 +0200

    Update data_io.py
    09.05.25-02:30'da update yaptim,
    read_json_file(filepath)
    write_json_file(filepath, data)
    append_to_json_file(filepath, new_data) ekledim

--- core/data_io.py
+++ core/data_io.py
@@ -1,7 +1,26 @@
 import json
-
+#Json dosayasini oku
 def read_json(filepath):
-    pass
+    if not os.path.exists(filepath):
+        return []  # Dosya yoksa boş liste döndür
+    try:
+        with open(filepath, 'r', encoding='utf-8') as file:
+            return json.load(file)
+    except json.JSONDecodeError:
+        return []  # JSON hataliysa boş liste döndür
 
+#json dosyasina yaz
 def write_json(filepath, data):
-    pass
+     with open(filepath, 'w', encoding='utf-8') as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
+
+
+# JSON dosyasına veri ekle (liste yapısında ise)
+def append_to_json_file(filepath, new_data):
+    data = read_json_file(filepath)
+    if isinstance(data, list):
+        data.append(new_data)
+    else:
+        data = [new_data]
+    write_json_file(filepath, data)
+    
