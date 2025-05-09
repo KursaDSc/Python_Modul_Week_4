d05d8ac47113f32eff5892c89a36316064b883b6
FurkanBilal <furkanbilalcubuk@gmail.com>
2025-05-09 12:06:16 +0200

    Furkan
    aciklamalar

--- book_transactions.py
+++ book_transactions.py
@@ -4,7 +4,7 @@ import core.data_io as di
 #kitaplarin kayitli oldugu dosya
 books_file = 'data/books.json'
 
-
+#kitap ekleme
 def add_book(barcode, title , publisher, author, status):
     books = di.read_json(books_file)
     books.append({
@@ -15,12 +15,12 @@ def add_book(barcode, title , publisher, author, status):
         'status': status
     })
     di.write_json(books_file, books)
-
+#kitap silme
 def delete_book(barcode):
     books = di.read_json(books_file)
     books = [book for book in books if book['barcode'] != barcode]
     di.write_json(books_file, books)
-
+#kitap silme
 def search_book(search_term):
     books = di.read_json(books_file)
     results = []
@@ -30,11 +30,11 @@ def search_book(search_term):
             search_term.lower() in book['publisher'].lower()):
             results.append(book)
     return results
-
+#kitaplari listeleme
 def get_all_books():
     books = di.read_json(books_file)
     return books
-
+#kitap durumunu kontrol etme
 def is_book_available(barcode):
     books = di.read_json(books_file)
     for book in books:
